cc7530df03ecc3481e803c0521bf4603f1bfe807
nori <norinorivertech@gmail.com>
2022-06-21 08:47:11 +0900

    created get, create, post method restapi.

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,32 +1,31 @@
-from asyncio import tasks
+from http.client import HTTPException
 from typing import List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
+import api.cruds.task as task_crud
 import api.schemas.task as task_schema
+from api.db import get_db
 
 router = APIRouter()
 
 @router.get("/tasks", response_model=List[task_schema.Task])
-async def list_tasks():
-    return [task_schema.Task(id=1, title="1つ目のTODOタスク")]
+async def list_tasks(db: AsyncSession = Depends(get_db)):
+    return await task_crud.get_tasks_with_done(db)
 
 @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
-async def create_task(task_body: task_schema.TaskCreate):
-    return task_schema.TaskCreateResponse(id=1, **task_body.dict())
+async def create_task(task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)):
+    return await task_crud.create_task(db, task_body)
 
-@router.put("tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
-async def update_task(task_body: task_schema.TaskCreate):
-    return task_schema.TaskCreateResponse(id=1, **task_body.dict())
+@router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
+async def update_task(task_id: int, task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)):
+    task = await task_crud.get_task(db, task_id=task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    
+    return await task_crud.update_task(db, task_body, original=task)
 
-@router.delete("/tasks/{task_id}", response_model=None)
+@router.delete("/tasks/{task_id}")
 async def delete_task():
-    return 
-
-@router.put("/tasks/{task_id}/done", response_model=None)
-async def mark_task_as_done(task_id: int):
-    return
-
-@router.delete("tasks/{task_id}/done", response_model=None)
-async def unmark_task_as_done(task_id: int):
-    return
+    pass
