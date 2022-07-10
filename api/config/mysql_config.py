c30535d290be5b424fcc80f074ed962c165f66e8
nori <norinorivertech@gmail.com>
2022-07-10 10:21:20 +0900

    clearly code format.

--- api/config/mysql_config.py
+++ api/config/mysql_config.py
@@ -9,7 +9,7 @@ class DBConfig:
     CHARSET=os.environ.get("MYSQL_CONNECTION_CHARSET")
 
     def __init__(self):
-        self.DB_SETTING = "{0}:{1}@{2}:{3}/{4}?charset={5}".format(self.USER_NAME, self.PASSWORD, self.HOSTNAME, self.PORT,self.DB_NAME ,self.CHARSET)
+        self.DB_SETTING = "{0}:{1}@{2}:{3}/{4}?charset={5}".format(self.USER_NAME, self.PASSWORD, self.HOSTNAME, self.PORT, self.DB_NAME ,self.CHARSET)
         
     def __str__(self):
         return self.DB_SETTING
